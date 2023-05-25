5fc7506dbbc91774c004b259ef4ff44bff434359
Kevin Svitač <kevin@svit.ac>
2023-05-25 18:19:38 +0200

    Remove debug console log

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,7 +1,6 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('fof/discussion-thumbnail', () => {
-    console.log('aaaa');
     app.extensionData.for('fof-discussion-thumbnail').registerSetting({
         label: app.translator.trans('fof-discussion-thumbnail.admin.settings.link_to_discussion_label'),
         setting: 'fof-discussion-thumbnail.link_to_discussion',
