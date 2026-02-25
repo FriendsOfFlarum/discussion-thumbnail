8d5ec8f43265455407fc5d93006965b650ca32d9
IanM <ian@morland.me>
2026-02-25 02:16:57 +0000

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,7 +1,7 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('fof/discussion-thumbnail', () => {
-  app.extensionData.for('fof-discussion-thumbnail').registerSetting({
+  app.registry.for('fof-discussion-thumbnail').registerSetting({
     label: app.translator.trans('fof-discussion-thumbnail.admin.settings.link_to_discussion_label'),
     setting: 'fof-discussion-thumbnail.link_to_discussion',
     type: 'boolean',
